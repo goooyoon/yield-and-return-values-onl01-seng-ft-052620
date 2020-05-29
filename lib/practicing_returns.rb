8c723484c26aa126ac210467e20adfe996dbb740
Grace Kim <goooyoon@gmail.com>
2020-05-29 02:03:48 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,15 +1,6 @@
 require 'pry'
 
 def hello(array)
-  i = 0
-  while i < array.length
-    binding.pry
-    yield(array[i])
-    i += 1
-  end
-end
-
-def hello(array)
   i = 0	
   collection = []
   while i < array.length
