a9023064e06f13f0b64a85bb9b281ca103badcbe
Grace Kim <goooyoon@gmail.com>
2020-05-29 01:59:45 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -10,3 +10,14 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+def hello(array)
+  i = 0	  i = 0
+  collection = []
+  while i < array.length	  while i < array.length
+    yield(array[i])	    collection << yield(array[i])
+    i += 1	    i += 1
+  end	  end
+  collection
+end	end
